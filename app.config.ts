f21e35c83b644ebbbf2075009f449cfe41bd2935
antoine MAHASSADI <macbook@MBP-dantoine.lan>
2025-04-29 20:28:47 +0200

    feat: change color theme to new palette (#1)

---
+++ app.config.ts
@@ -0,0 +1,13 @@
+export default defineAppConfig({
+  ui: {
+    colors: {
+      primary: 'teal',
+      secondary: 'violet',
+      success: 'lime',
+      info: 'cyan',
+      warning: 'amber',
+      error: 'pink',
+      neutral: 'stone'
+    }
+  }
+}) 
