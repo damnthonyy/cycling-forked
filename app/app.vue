bb5011e42747161631bd0ec496cdcc9f17963332
antoine MAHASSADI <macbook@MBP-dantoine.lan>
2025-04-29 20:15:03 +0200

    feat: replace GithubIcon URL with current repo URL (#5)

--- app/app.vue
+++ app/app.vue
@@ -32,8 +32,7 @@ useSeoMeta({
     <UHeader>
       <template #left>
         <NuxtLink to="/">
-          <!-- <LogoPro class="w-auto h-6 shrink-0" /> -->
-          <!-- <h1>Cycling</h1> -->
+          
            <img src="https://avatars.githubusercontent.com/u/189975276?s=200&v=4" class="w-auto h-6 shrink-0 w-10 h-10 bg-gray-200" />
         </NuxtLink>
 
@@ -44,7 +43,7 @@ useSeoMeta({
         <UColorModeButton />
 
         <UButton
-          to="https://github.com/your-username/cycling"
+          to="https://github.com/edumapper/cycling"
           target="_blank"
           icon="i-simple-icons-github"
           aria-label="GitHub"
